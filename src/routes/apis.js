5cfd58eeb6ba6f05089a89ada3b3c7a7466f01db
unnKoel <unnkoel@gmail.com>
2018-06-19 19:46:45 +0800

    add head/stable set api

--- src/routes/apis.js
+++ src/routes/apis.js
@@ -4,6 +4,7 @@ import apiStatusStore from "../store/apiStatus";
 import apiStackStore from "../store/apiStack";
 import { transferTemplate } from "../utils";
 var router = express.Router();
+var jsondiffpatch = require("jsondiffpatch");
 
 /* GET apis list */
 router.get("/", function(req, res, next) {
@@ -49,10 +50,15 @@ router.get("/stack", function(req, res, next) {
 
 /* GET apis head stack info */
 router.get("/stack/head", function(req, res, next) {
-  const { workProject, url, method, code, desc } = req.query;
+  const { workProject, url, method, code, desc, set } = req.query;
   const api = apiStore.getOne(workProject, url, method);
+  const numberCode = parseInt(code);
   if (api) {
-    const apiStatus = apiStatusStore.getOneByApiStatus(api.id, parseInt(code));
+    let apiStatus = apiStatusStore.getOneByApiStatus(api.id, numberCode);
+    if (set) {
+      apiStatusStore.updateHead(apiStatus.id, numberCode, set);
+      apiStatus = apiStatusStore.getOneByApiStatus(api.id, numberCode);
+    }
     if (desc) {
       res.json(
         transferTemplate(
@@ -69,10 +75,15 @@ router.get("/stack/head", function(req, res, next) {
 
 /* GET apis stable stack info */
 router.get("/stack/stable", function(req, res, next) {
-  const { workProject, url, method, code, desc } = req.query;
+  const { workProject, url, method, code, desc, set } = req.query;
   const api = apiStore.getOne(workProject, url, method);
+  const numberCode = parseInt(code);
   if (api) {
-    const apiStatus = apiStatusStore.getOneByApiStatus(api.id, parseInt(code));
+    let apiStatus = apiStatusStore.getOneByApiStatus(api.id, numberCode);
+    if (set) {
+      apiStatusStore.updateStable(apiStatus.id, numberCode, set);
+      apiStatus = apiStatusStore.getOneByApiStatus(api.id, numberCode);
+    }
     if (desc) {
       res.json(
         transferTemplate(
@@ -87,14 +98,20 @@ router.get("/stack/stable", function(req, res, next) {
   }
 });
 
+/* GET diff between the two apis stack */
+router.get("/stack/diff", function(req, res, next) {
+  const { newHash, oldHash } = req.query;
+  const delta = jsondiffpatch.diff(
+    apiStackStore.getStackById(newHash).result,
+    apiStackStore.getStackById(oldHash).result
+  );
+  res.send(delta);
+});
+
 /* GET apis stack info */
 router.get("/stack/:hash", function(req, res, next) {
   const { hash } = req.params;
   res.json(transferTemplate(apiStackStore.getStackById(hash)));
-});
-
-router.get("/stack/diff", function(req, res, next) {
-  const { newHash, oldHash } = req.query;
 });
 
 export default router;
